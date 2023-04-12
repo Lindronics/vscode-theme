c526e852498f6efb5ed72b186d8f0d7631ac64a8
Niklas Lindorfer <niklas@lindorfer.com>
2023-04-12 22:21:42 +0100

    Modified theme

--- samples/rust_test/src/main.rs
+++ samples/rust_test/src/main.rs
@@ -2,12 +2,32 @@
 pub async fn main() {
     #[allow(clippy::disallowed_names)]
     let foo = "bar";
-    println!("Hello {foo}");
-    fizz();
+
+    for _ in 1..100 {
+        println!("Hello {foo}");
+    }
+
+    let x = 5;
+    match fizz(x).await {
+        Some(_buzz) => {
+            // asdf
+            todo!()
+        }
+        None => {
+            todo!()
+        }
+    };
 }
 
-fn fizz() -> Option<Buzz> {
+/// does some stuff
+async fn fizz(_x: u16) -> Option<Buzz<u16>> {
     None
 }
 
-struct Buzz {}
+#[derive(Clone)]
+struct Buzz<T>
+where
+    T: Clone,
+{
+    _inner: T,
+}
